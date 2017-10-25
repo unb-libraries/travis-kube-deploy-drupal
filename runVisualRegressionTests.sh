8de72e15052035d162edc3c33f75338a19c3ecf7
Jacob Sanford <jsanford@unb.ca>
2017-10-25 12:23:33 -0300

    Add conditional exit to regression

--- runVisualRegressionTests.sh
+++ runVisualRegressionTests.sh
@@ -4,6 +4,12 @@ set -e
 if [ -f ./tests/backstop/backstop.json ]; then
   echo "Running Regression Tests in /tests/backstop..."
   docker run --rm -v $(pwd)/tests/backstop:/src docksal/backstopjs test
+  TEST_RETURN_CODE=$?
+  if [ $TEST_RETURN_CODE != 0 ]; then
+    echo "Visual Regression Tests Report Failures. Exiting."
+    exit 1
+  fi
+  echo "Visual Regression Tests Passed."
 else
   echo "No Visual Regression Tests in /tests/backstop."
 fi
