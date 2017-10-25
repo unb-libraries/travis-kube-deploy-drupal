cb1723ab8ed5581e6952d07772a75ac0082b1af5
Jacob Sanford <jsanford@unb.ca>
2017-10-25 13:19:19 -0300

    Update regression test order

--- runVisualRegressionTests.sh
+++ runVisualRegressionTests.sh
@@ -3,12 +3,19 @@ set -e
 
 if [ -f ./tests/backstop/backstop.json ]; then
   echo "Running Regression Tests in /tests/backstop..."
+
+  echo "Pulling Upstream Image"
+  git pull docksal/backstopjs
+
+  echo "Running Tests"
   docker run --rm -v $(pwd)/tests/backstop:/src docksal/backstopjs test
+
   TEST_RETURN_CODE=$?
   if [ $TEST_RETURN_CODE != 0 ]; then
     echo "Visual Regression Tests Report Failures. Exiting."
     exit 1
   fi
+
   echo "Visual Regression Tests Passed."
 else
   echo "No Visual Regression Tests in /tests/backstop."
