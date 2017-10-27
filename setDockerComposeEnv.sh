6b30e634d66f761a1450bef6a3eb6cc8d35fdbc1
Jacob Sanford <jsanford@unb.ca>
2017-10-27 12:42:07 -0300

    Quote variable to expand it

--- setDockerComposeEnv.sh
+++ setDockerComposeEnv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-sed -i 's|DEPLOY_ENV=local|DEPLOY_ENV=$TRAVIS_BRANCH|g' env/drupal.env
+sed -i "s|DEPLOY_ENV=local|DEPLOY_ENV=$TRAVIS_BRANCH|g" env/drupal.env
 cat env/drupal.env
