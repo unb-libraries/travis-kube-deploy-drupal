6a8980a6a2fe1a817fa8b9dd52a5d1af015131e1
Jacob Sanford <jsanford@unb.ca>
2017-10-27 13:04:54 -0300

    Use proper URI formalism for dev services

--- runVisualRegressionTests.sh
+++ runVisualRegressionTests.sh
@@ -12,7 +12,7 @@ if [ -f ./tests/backstop/backstop.json ]; then
   KUBE_DEPLOYMENT_NAME=$(echo $SERVICE_NAME | sed 's/\./-/g')
   POD_NAME=$(kubectl get pods --namespace=$BRANCH -l tier=$KUBE_DEPLOYMENT_NAME | grep Running | awk '{ print $1 }' | head -n 1)
   DEV_IP=$(kubectl exec $POD_NAME --namespace=$BRANCH curl ipinfo.io/ip)
-  docker run --rm --add-host dev$SERVICE_NAME:$DEV_IP -v $(pwd)/tests/backstop/$BRANCH:/src docksal/backstopjs test
+  docker run --rm --add-host dev-$SERVICE_NAME:$DEV_IP -v $(pwd)/tests/backstop/$BRANCH:/src docksal/backstopjs test
 
   TEST_RETURN_CODE=$?
   if [ $TEST_RETURN_CODE != 0 ]; then
