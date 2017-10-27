257612e5b35d55390511a602a59949e0b001f189
Jacob Sanford <jsanford@unb.ca>
2017-10-27 12:21:05 -0300

    Convert to per-branch job builds

--- runVisualRegressionTests.sh
+++ runVisualRegressionTests.sh
@@ -8,7 +8,11 @@ if [ -f ./tests/backstop/backstop.json ]; then
   docker pull docksal/backstopjs
 
   echo "Running Tests"
-  docker run --rm -v $(pwd)/tests/backstop:/src docksal/backstopjs test
+
+  KUBE_DEPLOYMENT_NAME=$(echo $SERVICE_NAME | sed 's/\./-/g')
+  POD_NAME=$(kubectl get pods --namespace=$BRANCH -l tier=$KUBE_DEPLOYMENT_NAME | grep Running | awk '{ print $1 }' | head -n 1)
+  DEV_IP=$(kubectl exec $POD_NAME --namespace=$BRANCH curl ipinfo.io/ip)
+  docker run --rm --add-host dev$SERVICE_NAME:$DEV_IP -v $(pwd)/tests/backstop/$BRANCH:/src docksal/backstopjs test
 
   TEST_RETURN_CODE=$?
   if [ $TEST_RETURN_CODE != 0 ]; then
